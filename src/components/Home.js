286e13e5bea9bb786cbf679a65d80160794b5075
Bryony McLaughlin <bryonymc@gmail.com>
2016-11-18 10:13:11 -0600

    updated language on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,7 +16,7 @@ class Home extends Component {
       <div className="homepage-content" >
       <h1>Welcome to Sandbox</h1>
       <br/>
-      <p>Sandbox is an educational tool which allows users to familiarize themselves with different CSS properties. With Sandbox, a user can manipulate text and images, which allows them to see how different properties interact, while also rendering the snipet of code specific to their changes in real time.</p>
+      <p>Sandbox is an educational tool which allows users to familiarize themselves with different CSS properties. A user can manipulate text and images with Sandbox, showing them how different properties effect elements, while also rendering the code specific to their changes in real time.</p>
       </div>
     )
   }
